45bc899d2a8b90ac17797e992fa48db6ef213d9f
dominiclizarraga <dominiclizarraga@hotmail.com>
2026-02-02 11:25:29 -0600

    First commit (chapter 5)

---
+++ chapter_5/exercise_5.4.rb
@@ -0,0 +1,17 @@
+Exercise 5.4: Smells and Refactorings
+Consider these smells:
+A. Comments
+B. Large Module
+C. Long Method
+D. Long Parameter List
+For each refactoring in the following list, write the letter for the smell(s) it might
+help cure:
+___ Duplicate Observed Data
+___ Extract Class
+___ Extract Method
+___ Extract Subclass
+___ Introduce Assertion
+___ Introduce Parameter Object
+___ Preserve Whole Object
+___ Rename Method
+___ Replace Parameter with Method
